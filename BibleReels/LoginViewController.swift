8a7fede2458ff44b3c3e62911ffeb8bb49e95458
Phanuelle Manuel <phanmanuel9@gmail.com>
2025-04-26 00:08:45 -0500

    created project and moved it out of duplicate folder

---
+++ BibleReels/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  BibleReels
+//
+//  Created by Phanuelle Manuel on 4/26/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Login"
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BibleReels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BibleReels
+//
+//  Created by Phanuelle Manuel on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
